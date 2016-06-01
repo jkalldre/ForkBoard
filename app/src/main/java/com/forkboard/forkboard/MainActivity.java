548b2ecd14e1c38c1112c34090cdb7138e0095c1
jkalldre <jakekalldredge@gmail.com>
2016-06-01 10:01:12 -0600

    button1 now changes background color to cyan

--- app/src/main/java/com/forkboard/forkboard/MainActivity.java
+++ app/src/main/java/com/forkboard/forkboard/MainActivity.java
@@ -1,7 +1,11 @@
 package com.forkboard.forkboard;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeBackground(View v) {
+        RelativeLayout rl = (RelativeLayout)findViewById(R.id.background);
+        rl.setBackgroundColor(Color.CYAN);
     }
 }
