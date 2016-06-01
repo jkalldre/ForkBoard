928582528b47ecb10dadf08c97fbf291d4a76cea
jkalldre <jakekalldredge@gmail.com>
2016-06-01 14:14:25 -0600

    added calendar view and changed default color of actionbar

--- app/src/main/java/com/forkboard/forkboard/MainActivity.java
+++ app/src/main/java/com/forkboard/forkboard/MainActivity.java
@@ -1,22 +1,27 @@
 package com.forkboard.forkboard;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static int myCOLOR = Color.parseColor("#fef2b9");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // ActionBar bar = getSupportActionBar();
+      //  bar.setBackgroundDrawable(new ColorDrawable(myCOLOR));
     }
 
     public void changeBackground(View v) {
         RelativeLayout rl = (RelativeLayout)findViewById(R.id.background);
-        rl.setBackgroundColor(Color.CYAN);
+        rl.setBackgroundColor(myCOLOR);
+
     }
 }
