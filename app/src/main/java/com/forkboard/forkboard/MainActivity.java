b086aa2c9fafdc61c9d920712cdf713e0a69697b
jkalldre <jakekalldredge@gmail.com>
2016-06-06 09:49:36 -0600

    testing UI

--- app/src/main/java/com/forkboard/forkboard/MainActivity.java
+++ app/src/main/java/com/forkboard/forkboard/MainActivity.java
@@ -7,13 +7,25 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar tb = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(tb);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_nav);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.menu, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
