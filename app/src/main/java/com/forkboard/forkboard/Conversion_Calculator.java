8321676a6812c0535df6edb14788a29dd73602af
jkalldre <jakekalldredge@gmail.com>
2016-06-16 19:28:20 -0600

    ui update

--- app/src/main/java/com/forkboard/forkboard/Conversion_Calculator.java
+++ app/src/main/java/com/forkboard/forkboard/Conversion_Calculator.java
@@ -3,18 +3,21 @@ package com.forkboard.forkboard;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class Conversion_Calculator extends Activity {
+public class Conversion_Calculator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversion__calculator);
-
+        Toolbar tb = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(tb);
         String[] arraySpinner = {"tsp", "cup", "oz"};
         Spinner s = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
