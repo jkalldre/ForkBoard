666686129e6a38abc707753487dfff98bb329b21
jaortiz9 <ortiz.janine@gmail.com>
2016-07-12 19:33:25 -0600

    Get Started main Activity

--- app/src/main/java/com/forkboard/forkboard/GetStarted.java
+++ app/src/main/java/com/forkboard/forkboard/GetStarted.java
@@ -3,6 +3,7 @@ package com.forkboard.forkboard;
 
 import android.content.Intent;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 /**
@@ -10,8 +11,13 @@ import android.webkit.WebView;
  */
 public class GetStarted extends AppCompatActivity {
 
-   // WebView webView = new WebView(this);
-   //// webView.getSettings().setJavaScriptEnabled(true);
-    String urlDemo = "http://www.appdemostore.com/m/4934128529047552";
- //   webView.loadUrl(urlDemo);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        String urlDemo = "http://www.appdemostore.com/m/4934128529047552";
+        webView.loadUrl(urlDemo);
+
+    }
 }
