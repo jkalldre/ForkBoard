c66ea838a13d4ae7bd87320c10f8c0719b5225e8
jaortiz9 <ortiz.janine@gmail.com>
2016-07-12 18:40:05 -0600

    Get Started main Activity

--- app/src/main/java/com/forkboard/forkboard/GetStarted.java
+++ app/src/main/java/com/forkboard/forkboard/GetStarted.java
@@ -1,9 +1,15 @@
 package com.forkboard.forkboard;
 
+
+import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 /**
  * Created by JamJam on 7/12/16.
  */
-public class GetStarted {
+public class GetStarted extends AppCompatActivity {
+
     WebView webView = new WebView(this);
     webView.getSettings().setJavaScriptEnabled(true);
     String urlDemo = "http://www.appdemostore.com/m/4934128529047552";
