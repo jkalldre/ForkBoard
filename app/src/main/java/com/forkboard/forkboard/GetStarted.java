14461804f36a7fb51af31dc5ca2c766b4d5dfe65
jaortiz9 <ortiz.janine@gmail.com>
2016-07-13 18:59:03 -0600

    Get Started Demo

--- app/src/main/java/com/forkboard/forkboard/GetStarted.java
+++ app/src/main/java/com/forkboard/forkboard/GetStarted.java
@@ -1,23 +1,52 @@
 package com.forkboard.forkboard;
 
 
-import android.content.Intent;
-
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebView;
+import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+
 /**
  * Created by JamJam on 7/12/16.
+ * Janine used
+ * http://stackoverflow.com/questions/5026349/how-to-open-a-website-when-a-button-is-clicked-in-android-application
  */
 public class GetStarted extends AppCompatActivity {
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.get_started);
 
-        WebView webView = new WebView(this);
-        webView.getSettings().setJavaScriptEnabled(true);
-        String urlDemo = "http://www.appdemostore.com/m/4934128529047552";
-        webView.loadUrl(urlDemo);
+        Toolbar tb =  (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(tb);
 
+        Button btn = (Button) findViewById(R.id.btn_clickme);
+        btn.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("http://www.appdemostore.com/m/4934128529047552"));
+                startActivity(myWebLink);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected (MenuItem item){
+        new ActivityChanger().changeActivity(item,this);
+        return true;
     }
 }
